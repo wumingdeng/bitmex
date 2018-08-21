bf68d2e5db90170411918d00933dcd842e14f2aa
fizzo_mac <809050631@qq.com>
2018-08-21 11:23:49 +0800

    11

--- unit/test_v2.js
+++ unit/test_v2.js
@@ -1,15 +1,3 @@
-let obj = {
-    a:'djdj',
-    b:'djdj',
-    c:'djdj',
-    d:'djdj',
-    e:'djdj',
-    f:'djdj'
-}
-delete obj.f
-console.log(obj)
-delete obj.g
-console.log(obj)
-obj.g = 'jdjdj'
-console.log(obj)
+let  a = '-1223232'
+console.log(a.includes('+'))
 
--- util/index.js
+++ util/index.js
@@ -3,7 +3,9 @@ const crypto = require('crypto');
 const qs = require('qs')
 const cfg = require('../config')
 const g = require('../global')
-const test_count = 0
+var test_count = 0
+var test_Arr = [0, 7, 1]
+const TRADE_DEBUG = true //不交易 ，测试时候用，
 module.exports = {
     //获取订单的状态
     getOrderState: async function (apiKey, apiSecret, data) {
@@ -68,7 +70,6 @@ module.exports = {
                 method: verb,
                 body: postBody
             };
-
             request(requestOptions, function (error, response, body) {
                 if (error) {
                     reject(error);
@@ -84,14 +85,14 @@ module.exports = {
         let self = $self ? $self : this
         return new Promise((resolve, reject) => {
             let headers = {
-                'X-TOKEN': '0b7ced7f88d852c8c0f5dea70a3d748axqBNmuz28wfYe0zS1pT4b1fsXRDbRlYU9ZaHjl8/CbxMmCQkzkuBe3OJvV+b0kHzj8l3',
-                'X-SIGN': 'F24D4CCFB6E989F8F2525CB89A1D66D4',
-                'X-TS': 1533523833,
+                'X-TOKEN': '4feaf6d8937eeba020339d9618830e6bIlMSCpP9aLDBkkHAMVRbCFfr7hPWeysFgdGJ7Zrbj7aOEq3W4NkKUmEou7yf3Bh8qBIT',
+                'X-SIGN': '0AE62D23CC94AA91BFA02BB92D0454C7',
+                'X-TS': 1534254436,
                 'X-APPID': 'toBPJhUqos'
             };
             const requestOptions = {
                 headers: headers,
-                url: 'https://xkt.sftui.com/api/v1/feedTrade/getTradeList?page=1&page_size=3',
+                url: 'https://api.xkt.one/api/v1/analyst/getAnalystTradeList?analyst_id=100003&page=1&page_size=20',
                 method: 'GET',
             };
             request(requestOptions, function (error, response, body) {
@@ -100,49 +101,44 @@ module.exports = {
                 } else {
                     if (self.isString(body)) body = JSON.parse(body)
                     if (body['data'] && body['data']['data_list']) {
-                        body = body['data']['data_list'][test_count]
+                        body = body['data']['data_list']
                     } else {
-                        console.log(body)
                         resolve(body);
                         return
                     }
                     resolve(body);
-                    if(test_count > 2) return
+                    body = body[test_Arr[test_count]]
+                    if (test_count > 2) return
                     if (!g.feedid[data_order.apikey]) {
                         g.feedid[data_order.apikey] = body.feed_id
                     }
                     console.log('g.feedid[apikey]', g.feedid[data_order.apikey])
                     console.log('body.feedid', body.feed_id)
                     if (true) {
-                    // if (body.feed_id > g.feedid[data_order.apikey]) {
+                        // if (body.feed_id > g.feedid[data_order.apikey]) {
                         g.feedid[data_order.apikey] = body.feed_id
-                        if (body.order.is_close === '1') {
+                        if (body.order.is_close === 1) {
                             data_order.execInst = 'Close'
                             delete data_order.side
                             delete data_order.orderQty
-                        } else if (body.order.is_close === '0') {
-                            delete data_order.execInst 
+                        } else if (body.order.is_close === 0) {
+                            delete data_order.execInst
                             data_order.orderQty = 1
-                            if (body.order.trade_qty.include('+')) {
+                            if (body.order.trade_qty.includes('+')) {
                                 data_order.side = 'Buy'
                             } else {
                                 data_order.side = 'Sell'
                             }
                         }
                         console.log('new order', data_order)
-                        if(data_order.side === 'Buy'){
-                            data_order.side = 'Sell'
-                        }else{
-                            data_order.side = 'Buy'
-                        }
-                        setTimeout(function(){
+                        setTimeout(function () {
                             test_count++
                             self.onHandOrder(data_order)
-                        },5000)
+                        }, 1000)
                     } else {
                         setTimeout(function () {
                             self.getHandOrder(data_order, self)
-                        }, 5*60*1000)
+                        }, 5 * 60 * 1000)
                     }
                 }
             });
@@ -151,20 +147,28 @@ module.exports = {
     //处理订单
     //
     onHandOrder: async function (data_order) {
-        // let order = await this.createOrder(data_order.apikey, data_order.apiSecret, data_order).then(res => {
-        //     return res
-        // }, err => {
-        //     return null
-        // })
-        // if (order) {
-        //     let orderid = order.orderID
+        let order = {}
+        if (!TRADE_DEBUG) {
+            order = await this.createOrder(data_order.apikey, data_order.apiSecret, data_order).then(res => {
+                return res
+            }, err => {
+                console.log(err)
+                return null
+            })
+        }
+        if (order) {
+            let orderid = order.orderID
             let data_sym = { symbol: data_order.symbol };
             let $state = await this.getOrderState(data_order.apikey, data_order.apiSecret, data_sym).then(res => {
-                for (let i in res) {
-                    let order = res[i]
-                    // if (order.orderID === orderid) {
-                        return order.ordStatus
-                    // }
+                if(TRADE_DEBUG){
+                    return res[0].ordStatus
+                }else{
+                    for (let i in res) {
+                        let order = res[i]
+                        if (order.orderID === orderid) {
+                            return order.ordStatus
+                        }
+                    }
                 }
                 return null
             }, err => {
@@ -176,9 +180,9 @@ module.exports = {
                 return null
             })
             return { ok: 1, state: $state, h: hand }
-        // } else {
-        //     return { ok: 0 }
-        // }
+        } else {
+            return { ok: 0 }
+        }
     },
     //判断是否为字符串
     isString: function (source) {
